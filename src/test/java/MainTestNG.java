36738eb6e8fa8fb0b4610a4ff81dea09b8f8f37f
Mika <asthun@gmail.com>
2022-07-19 20:17:12 +0200

    annotations added

--- src/test/java/MainTestNG.java
+++ src/test/java/MainTestNG.java
@@ -1,7 +1,27 @@
 import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class MainTestNG {
+
+    @BeforeTest
+    public void printBeforeAllTest () {
+        System.out.println("Before all tests ....");
+    }
+
+    @AfterTest
+    public void printAfterAllTest () {
+        System.out.println("After all tests ....");
+    }
+
+    @BeforeMethod
+    public void printBeforeMethod () {
+        System.out.println("This is text before each test");
+    }
+
+    @AfterMethod
+    public void printAfterMethod () {
+        System.out.println("This is text after each test");
+    }
 
     @Test
     public void checkTicketNumberNotZeros (){
@@ -9,4 +29,5 @@ public class MainTestNG {
         int actual = Main.countTicketNumbers(0,0,0,0,0,1);
         Assert.assertTrue(actual >= expected, "Not all zeros.");
     }
+
 }
